cb9b2567c531c5ee4c8b0415bdd1806fff042dfc
Letícia Soares Machado <leticia.machado@aluno.ifsp.edu.br>
2022-12-28 12:12:24 -0300

    abstraindo um bootcamp utilizando java e OO
    projeto desenvolvido no bootcamp quality assurance para mulheres da DIO

---
+++ Abstraindo-um-Bootcamp-com-OO/src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,28 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso extends Conteudo{
+    private Integer cargaHoraria;
+
+    public Curso() {
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" + "titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + '}';
+    }
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO*cargaHoraria;
+    }
+    
+    
+}
